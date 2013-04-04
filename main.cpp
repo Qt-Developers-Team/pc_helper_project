ec0c4f0b20a68dfa9ecabb75810d5b5967168ab0
Roman <rok.n.rok.metal@gmail.com>
2013-04-04 14:58:05 +0600

    снова поменяли размер

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("windows-1251"));
     QApplication a(argc, argv);
     MyWidget w;
-    w.resize(860,500);
-    w.setMinimumSize(860,500);
+    w.resize(800,600);
+    w.setMinimumSize(800,600);
     w.setWindowTitle("PC Helper ver. 1.0");
     w.show();
     return a.exec();
